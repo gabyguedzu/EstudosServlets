b05b75cea2f77b90b7ab61968e4aeab07513ebc4
Gabriela Guedes <zooiv3rde@gmail.com>
2019-03-10 23:36:33 -0300

    criando ids das empredas

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,20 +6,32 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setNome("GuedesSoft");
+		empresa.setId(chaveSequencial++);
 		lista.add(empresa);
 	}
 
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);
 		
 	}
 	
 	public List<Empresa> getEmpresas() {
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		for (Empresa empresa : lista) {
+			if (empresa.getId() == id) {
+				lista.remove(empresa);
+			}
+		}
+		
 	}
 
 }
--- gerenciador/src/br/com/alura/gerenciador/servlet/ListaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/ListaEmpresaServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ListaEmpresaServlet
  */
-@WebServlet("/listaEmpresa")
+@WebServlet("/listaEmpresas")
 public class ListaEmpresaServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
