ec85ebcdba623b9a9f29865d6b91df2586be7a3f
Gabriela Guedes <zooiv3rde@gmail.com>
2019-03-20 22:31:53 -0300

    protegendo servlets

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -15,12 +15,6 @@ import br.com.alura.gerenciador.modelo.Empresa;
 public class ListaEmpresas implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
-		
 		Banco banco = new Banco();
 	    List<Empresa> lista = banco.getEmpresas();
 
--- gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
@@ -17,8 +18,18 @@ public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String paramAcao = request.getParameter("acao");
 
-		String paramAcao = request.getParameter("acao");		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoLogado = (sessao.getAttribute("usuarioLogado") == null);
+		boolean acaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+		
+		if(acaoProtegida && usuarioNaoLogado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+						
 		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;		
 		String nome;
 		
